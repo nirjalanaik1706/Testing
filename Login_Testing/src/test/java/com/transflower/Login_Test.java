61ffb931ff4713e69b91bbd7d575568d63b4ed3a
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-09-17 18:02:00 +0530

    First Login page testing mvn project

--- Login_Testing/src/test/java/com/transflower/Login_Test.java
+++ Login_Testing/src/test/java/com/transflower/Login_Test.java
@@ -1,12 +1,20 @@
 package com.transflower;
 
+import org.junit.jupiter.api.AfterAll;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import java.time.Duration;
 
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -25,16 +33,43 @@ public class Login_Test {
         WebElement username = driver.findElement(By.id("username"));
         WebElement password = driver.findElement(By.id("password"));
         WebElement login = driver.findElement(By.id("login"));
-        WebElement msg = driver.findElement(By.id("msg"));
+        //WebElement msg = driver.findElement(By.id("msg"));
 
         username.sendKeys("nirjalanaik@gmail.com");
+        try{
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException e){
+            System.out.println(e);
+        }
         password.sendKeys("nirjala@123");
+        try{
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }
+
+        String enteredUser=username.getAttribute("value");
+        String enteredPass=password.getAttribute("value");
+
         login.click();
 
-        // Just to simulate result, test sets msg field
-        msg.sendKeys("Login successful");
+            WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+            WebElement message=wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("msg"))
+            );
 
-        assertEquals("Login successful", msg.getAttribute("value"));
+            if(enteredUser.equals("nirjalanaik@gmail.com")&&enteredPass.equals("nirjala@123")){
+                message.sendKeys("Login successfull");
+            }
+            else{
+                message.sendKeys("invalid login");
+            }
+
+
+        assertTrue(message.getAttribute("value").contains("Login successfull"));
     }
 
     @AfterAll
