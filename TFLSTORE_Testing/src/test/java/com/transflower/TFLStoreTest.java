07063d1f3ffba83aad612885a5e8ef08210e2a41
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-09-21 16:23:14 +0530

    Tested tflstore html pages

--- TFLSTORE_Testing/src/test/java/com/transflower/TFLStoreTest.java
+++ TFLSTORE_Testing/src/test/java/com/transflower/TFLStoreTest.java
@@ -67,7 +67,8 @@ public class TFLStoreTest {
         try{
         Thread.sleep(2000); 
         }
-        catch(InterruptedException e){
+        catch(InterruptedException e)
+        {
             System.out.println(e);
         }
 
