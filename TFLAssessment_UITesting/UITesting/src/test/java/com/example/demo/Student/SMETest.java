d7062bddaa81e391de27336338058d8f99c761fd
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-10-07 19:17:24 +0530

    TFLAssessment UITesting

--- TFLAssessment_UITesting/UITesting/src/test/java/com/example/demo/Student/SMETest.java
+++ TFLAssessment_UITesting/UITesting/src/test/java/com/example/demo/Student/SMETest.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,7 +17,7 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SMETest {
-      WebDriver driver;
+    WebDriver driver;
 
     @BeforeClass
     public void setup() {
@@ -31,90 +30,103 @@ public class SMETest {
     @Test
     public void loginTest() {
         driver.get("http://127.0.0.1:5500/Home.html");
-        
-        WebElement login=driver.findElement(By.linkText("Login"));
+
+        WebElement login = driver.findElement(By.linkText("Login"));
         login.click();
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.urlContains("loginJWT.html"));
-        Assert.assertTrue(driver.getCurrentUrl().contains("loginJWT.html"),"Login page not load");
+        Assert.assertTrue(driver.getCurrentUrl().contains("loginJWT.html"), "Login page not loaded");
 
         WebElement email = driver.findElement(By.id("email"));
         WebElement password = driver.findElement(By.id("password"));
 
         email.sendKeys("nirjala.naik@example.com");
         password.sendKeys("12345");
-        WebElement submit=driver.findElement(By.cssSelector("button[type='submit']"));
+
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
         submit.click();
+
         wait.until(ExpectedConditions.urlContains("/SME/sme.html"));
         Assert.assertTrue(driver.getCurrentUrl().contains("/SME/sme.html"));
 
-        WebElement createTest=driver.findElement(By.id("createTestLink"));
+        WebElement createTest = driver.findElement(By.id("createTestLink"));
         createTest.click();
+
         wait.until(ExpectedConditions.urlContains("/SME/sme.html"));
         Assert.assertTrue(driver.getCurrentUrl().contains("/SME/sme.html"));
 
-        WebElement TestName=driver.findElement(By.id("testName"));
+        WebElement TestName = driver.findElement(By.id("testName"));
         TestName.sendKeys("ADVANCED JAVA");
-        WebElement Duration=driver.findElement(By.id("duration"));
+
+        WebElement Duration = driver.findElement(By.id("duration"));
         Duration.sendKeys("00:40:00");
 
-        WebElement Subject=driver.findElement(By.id("subjectSelect"));
+        try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        WebElement Subject = driver.findElement(By.id("subjectSelect"));
         Select subject = new Select(Subject);
         subject.selectByVisibleText("ADVJAVA");
 
-        WebElement SME=driver.findElement(By.id("smeSelect"));
+        try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        WebElement SME = driver.findElement(By.id("smeSelect"));
         Select sme = new Select(SME);
         sme.selectByVisibleText("sahil kamble");
-        
-        WebElement DateTime=driver.findElement(By.id("datetime-local"));
-        DateTime.sendKeys("07-10-2025T18:00");
 
-        WebElement passinglevel=driver.findElement(By.id("passingLevel"));
+        try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        WebElement DateTime = driver.findElement(By.id("scheduledDate"));
+        DateTime.sendKeys("2025-10-07T18:00");
+
+        try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        WebElement passinglevel = driver.findElement(By.id("passingLevel"));
         passinglevel.sendKeys("2");
 
-        WebElement selectlist=driver.findElement(By.id("questionsList"));
-        Select questions = new Select(selectlist);
-        questions.selectByVisibleText("Spring Boot is used for developing?");
-        questions.selectByVisibleText("How can a Spring Boot application be packaged and distributed?");
-        questions.selectByVisibleText("What AOP stands for?");
-        questions.selectByVisibleText("Which of the following is the correct name of React.js?");
-        questions.selectByVisibleText("How to use ref keyword in beans.xml?");
+        try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
 
-        WebElement createtest=driver.findElement(By.id("submitBtn"));
+        WebElement que1 = driver.findElement(By.cssSelector("input[value='6']"));
+        que1.click();
+        WebElement que2 = driver.findElement(By.cssSelector("input[value='7']"));
+        que2.click();
+        WebElement que3 = driver.findElement(By.cssSelector("input[value='8']"));
+        que3.click();
+        WebElement que4 = driver.findElement(By.cssSelector("input[value='9']"));
+        que4.click();
+        WebElement que5 = driver.findElement(By.cssSelector("input[value='10']"));
+        que5.click();
+
+        try { Thread.sleep(2000); 
+        }
+         catch (InterruptedException e) {
+           e.printStackTrace(); }
+
+        WebElement createtest = driver.findElement(By.id("submitBtn"));
         createtest.click();
+
+   
         wait.until(ExpectedConditions.urlContains("/SME/reviewtest.html"));
-        Assert.assertTrue(driver.getCurrentUrl().contains("/SME/reviewtest.html"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("/SME/reviewtest.html"), "Review Test page not loaded");
 
-        WebElement backtoedit=driver.findElement(By.id("submitBtn"));
-        backtoedit.click();
-
-        wait.until(ExpectedConditions.urlContains("/SME/createTestJWT.html?edit=true"));
-        Assert.assertTrue(driver.getCurrentUrl().contains("/SME/createTestJWT.html?edit=true"));
-        createtest.click();
-
-        wait.until(ExpectedConditions.urlContains("/SME/reviewtest.html"));
-        Assert.assertTrue(driver.getCurrentUrl().contains("/SME/reviewtest.html"));
-        WebElement confirmandcreate=driver.findElement(By.id("confirmBtn"));
+        WebElement confirmandcreate = driver.findElement(By.id("confirmBtn"));
         confirmandcreate.click();
-        wait.until(ExpectedConditions.urlContains("/SME/sme.html"));
-        Assert.assertTrue(driver.getCurrentUrl().contains("/SME/sme.html"));
-        WebElement GetQuestionAndcriteria=driver.findElement(By.id("checkQuestionAsPerCriteria"));
-        GetQuestionAndcriteria.click();
-        wait.until(ExpectedConditions.urlContains("SME/sme.html?msg=Test%20Created%20Successfully\\r\\n"));
-        Assert.assertTrue(driver.getCurrentUrl().contains("SME/sme.html?msg=Test%20Created%20Successfully\r\n"));
-        
-        WebElement selectSubject=driver.findElement(By.id("ddlSubjects"));
-        Select subject1 = new Select(selectSubject);
-        subject1.selectByVisibleText("COREJAVA");
 
-        WebElement selectEvaCriteria=driver.findElement(By.id("ddlCriteria"));
-        Select evacriteria = new Select(selectEvaCriteria);
-        evacriteria.selectByVisibleText("OOPS");
+        // wait.until(ExpectedConditions.urlContains("/SME/sme.html"));
+        // Assert.assertTrue(driver.getCurrentUrl().contains("/SME/sme.html"));
 
-        
-        
+        // WebElement GetQuestionAndcriteria = driver.findElement(By.id("checkQuestionAsPerCriteria"));
+        // GetQuestionAndcriteria.click();
 
+        // wait.until(ExpectedConditions.urlContains("SME/sme.html?msg=Test%20Created%20Successfully"));
+        // Assert.assertTrue(driver.getCurrentUrl().contains("SME/sme.html?msg=Test%20Created%20Successfully"));
 
-}
+        // WebElement selectSubject = driver.findElement(By.id("ddlSubjects"));
+        // Select subject1 = new Select(selectSubject);
+        // subject1.selectByVisibleText("COREJAVA");
+
+        // WebElement selectEvaCriteria = driver.findElement(By.id("ddlCriteria"));
+        // Select evacriteria = new Select(selectEvaCriteria);
+        // evacriteria.selectByVisibleText("OOPS");
+    }
 }
