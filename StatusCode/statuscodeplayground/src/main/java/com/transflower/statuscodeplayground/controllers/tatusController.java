d262dba5463c939b5b303a2421b5246ed1f5507e
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-10-09 15:13:47 +0530

    Status code

--- StatusCode/statuscodeplayground/src/main/java/com/transflower/statuscodeplayground/controllers/tatusController.java
+++ StatusCode/statuscodeplayground/src/main/java/com/transflower/statuscodeplayground/controllers/tatusController.java
@@ -1,10 +1,13 @@
 package com.transflower.statuscodeplayground.controllers;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/status")
@@ -47,7 +50,7 @@ public class tatusController {
     public ResponseEntity<Map<String, String>> notFound() {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(Map.of("error", "Resource not found (404 Not Found)"));
+                .body(Map.of("error", "Resource not found(404 Not Found)"));
     }
 
     @GetMapping("/servererror")
