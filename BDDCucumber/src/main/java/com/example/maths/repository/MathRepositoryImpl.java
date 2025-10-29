4288aa008c52525793de1ebc770bb2407bab7690
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-10-29 17:23:52 +0530

    changes

--- BDDCucumber/src/main/java/com/example/maths/repository/MathRepositoryImpl.java
+++ BDDCucumber/src/main/java/com/example/maths/repository/MathRepositoryImpl.java
@@ -2,9 +2,8 @@ package  com.example.maths.repository;
 
 public class MathRepositoryImpl implements MathRepository {
     @Override
-    public int getNumber() 
-    {
-        return 42; 
+    public int getNumber() {
+        return 42; // Example static number
     }
     
 }
